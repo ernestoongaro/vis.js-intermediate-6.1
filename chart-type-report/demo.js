69328aa724edac19a2af5b95f58015b65f044cbf
Ernesto Ongaro <ernestoongaro@gmail.com>
2015-08-27 17:45:04 +0100

    try and fix

--- chart-type-report/demo.js
+++ chart-type-report/demo.js
@@ -5,49 +5,45 @@ visualize({
         organization: "organization_1"
     }
 }, function (v) {
-
-    //render report from provided resource
-    v("#container").report({
+    var report = v.report({
         resource: "/public/Samples/Reports/States",
-        error: handleError
+        error: handleError,
+        container: "#container"
     });
 
     //show error
     function handleError(err) {
         alert(err.message);
-    }
-    
-    console.log(report.data().components;
-    
-     var component = data.components
-                            .filter(function (c) {
-                                   return c.name === chartName;
-                            })
+    };
 
-  window.barChart = function barChart() {   
-       report
-    .updateComponent("salesChart", { 
-          chartType: "Bar"
-    })
-    .done(function(){
-        alert("Chart type changed!");
-    })
-    .fail(function(err){
-        alert(err.message);
+
+    var component = data.components.filter(function (c) {
+        return c.name === chartName;
     });
+
+    window.barChart = function barChart() {
+        report.updateComponent("salesChart", {
+            chartType: "Bar"
+        })
+            .done(function () {
+            alert("Chart type changed!");
+        })
+            .fail(function (err) {
+            alert(err.message);
+        });
     }
-    
-  window.pieChart = function pieChart() {   
-       report
-    .updateComponent("salesChart", { 
-          chartType: "Pie"
-    })
-    .done(function(){
-        alert("Chart type changed!");
-    })
-    .fail(function(err){
-        alert(err.message);
-    });
+
+    window.pieChart = function pieChart() {
+        report.updateComponent("salesChart", {
+            chartType: "Pie"
+        })
+            .done(function () {
+            alert("Chart type changed!");
+        })
+            .fail(function (err) {
+            alert(err.message);
+        });
     }
+    console.log(report.data().components);
 
 });
